9c7176a240c550c18fa8ba655e4275352b68fb1a
anumaslam-789 <130720606+anumaslam-789@users.noreply.github.com>
2023-06-05 19:22:10 +0500

    checked by miss

---
+++ app.js
@@ -0,0 +1,106 @@
+////let x = 15;
+//let num= +prompt("enter a num")
+//if(num > x){console.log ("X is greater");}
+
+//else { console.log("X is smaller");
+
+//}
+
+//let city = "karachi";
+//if (userCity == city){
+ //   document.write("Welcome to Karachi");
+
+//}
+ //2. write a program to take inputt gender from user 
+ //let gender = "male";
+ //let userGender = prompt("Enter you gender?");
+ //if (userGender == gender)
+ //{
+   // document.write("Good Morning sir");
+ //}
+ //else(userGender != gender)
+ //{
+   // document.write("Good Morning Ma'am");
+ //}
+
+//.F part
+
+//if("car" < "cat")
+//alert("car is smaller thn cat ");
+
+  // 4.
+
+//let userFuel = +prompt("Enter your car's fuel");
+//if (userFuel < 0.25){ 
+  //  alert("Please refill fuel in your car")}
+
+ //5 .
+// var a = 4;
+ //if (++a === 5)
+ // {alert("given condition for variable a is true");}
+
+ // Bpart
+
+ //var b = 82;
+ //if(b++ === 83)
+ //{alert("given condition for variable a is true");}
+
+ // C part
+
+// var c = 12;
+ //if(c++ === 13)
+  //{alert(" condition 1 is true");}
+ // if(c === 13)
+ // {alert(" condition 2 is true");}
+ //if(++c < 14)
+ //{alert(" condition 2 is true");}
+
+ //if(c === 14)//
+ //{alert(" condition 3 is true");}
+
+
+ //D prt
+ //var b = 82;
+ //if(){}
+
+ //E prt
+ //var materialCost= 82;
+// var labourCost = 82;
+// var totalCost = (materialCost + labourCost)
+
+// if(totalCost === labourCost + materialCost)
+// {alert("the cost equals")}
+
+
+
+var obtMarks = +prompt ("Enter Obtained Marks");
+var totalMarks =  +prompt ("Enter Total Marks");
+ var percentage = (obtMarks/totalMarks) * 100;
+
+document.write("<h3>" + "Marksheet" + "</h3>");
+document.write("TOTAL MARKS :" +  totalMarks + "<br>");
+document.write("Marks Obtained:" + obtMarks + "<br>");
+document.write("percentage:" + percentage + "<br>");
+
+
+var Grade ;
+ var Remarks;
+ if(percentage >= 80)
+ { Grade = "A-One";
+      Remarks = "Excellent";}
+ else if(percentage >= 70){  Grade = "A"
+ Remarks = "Good"
+
+ }
+  else if(percentage >= 60)
+ { Grade =" B"
+ Remarks = "You need to impr0ve"}
+  else if(percentage > 60){ Grade = "Fail"
+ Remarks = "Sorrry"}
+ document.write("Grade:" + Grade + "<br>");
+document.write("Remarks:" + Remarks + "<br>");
+ 
+
+ 
+ 
+
